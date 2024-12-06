b3cf382a4e28200d9e6696c056e3c7d52fa2e86d
Jasper Slipman <jasperslipman@outlook.com>
2024-12-06 15:22:59 +0000

    NPM BUILD WORKING

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-  
-      return config;
-    },
-  };
+const nextConfig = {
+  webpack: (config) => {
+    console.log('Webpack Config:', JSON.stringify(config.module.rules, null, 2));
+    return config;
+  },
+};
+
+module.exports = nextConfig;
