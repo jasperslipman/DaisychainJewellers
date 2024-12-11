93030ac400a70eada3365beb1861ca95b95db2b2
Jasper Slipman <jasperslipman@outlook.com>
2024-12-11 17:47:39 +0000

    next.config.js updated for image optimization + Cashing Headers for st atic assets

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-const nextConfig = {
+// next.config.js
+
+module.exports = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -6,6 +8,34 @@ const nextConfig = {
     });
     return config;
   },
+  images: {
+    domains: ['main.d176itksgiqcki.amplifyapp.com'],
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+    minimumCacheTTL: 60,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(images|fonts)/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/(static|_next/static)/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      // Add more rules if necessary
+    ];
+  },
 };
-
-module.exports = nextConfig;
